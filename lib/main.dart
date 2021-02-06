e7c04cef36eebb8fc25b1c90c273158ad2de607d
Jorge Grajales <jorge.grajales@areamovil.com.co>
2021-02-06 10:55:09 -0500

    Codigo sesion 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,220 @@
 import 'package:flutter/material.dart';
-
-import 'Fibonacci.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.light
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeRoute()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeRoute extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("My app title")
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Text("Usuario Areamovil", style: TextStyle(fontSize: 20, color: Colors.white),),
+                decoration: BoxDecoration(
+                  color: Colors.blue
+                ),
+              ),
+              ListTile(
+                title: Text("Home"),
+              ),
+              ListTile(
+                title: Text("Profile"),
+                onTap: () => changeRoute(context)
+              )
+            ],
+          )
+        ),
+        body: ColumnWidget()
+      );
+  }
+
+  void changeRoute(BuildContext context)
+  {
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context) => ProfileRoute()
+    ));
+  }
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class ColumnWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [        
+        Container(
+          color: Colors.blue,
+          child: Text("Body", style: TextStyle(fontSize: 30)),
+          width: 100.0,
+          height: 50.0,
+          padding: EdgeInsets.all(5.0)
+        ),
+        Container(
+          color: Colors.red,
+          width: 100.0,
+          height: 100.0,
+          child: Image(image: NetworkImage("http://bit.ly/flutter_tiger"))
+        ),
+        Container(
+          width: 100.0,
+          height: 100.0,
+          child: Image(image: AssetImage("assets/images.png"))
+        )
+      ],
+    );
   }
+}
+
+class ProfileRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile")
+      ),
+      body: ColumnFormWidget()
+    );
+  }
+}
+
+class ColumnFormWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Perfil", style: TextStyle(fontSize: 20)),
+          InputTextWidget(TextInputType.text, "Name"),
+          InputTextWidget(TextInputType.emailAddress, "Email"),
+          InputTextWidget(TextInputType.number, "Phone"),
+          SizedBox(height: 20),
+          Text("Profesión"),
+          DropdownWidget(),
+          CheckboxInput(),
+          TextButton(
+            child: Text("Register"),
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blue,
+              primary: Colors.white
+            ),
+            onPressed: () {}
+          )
+        ],
+      )
+    );
+  }
+}
+
+class InputTextWidget extends StatefulWidget {
+
+  final TextInputType type;
+  final String description;
+
+  InputTextWidget(this.type, this.description);
+
+  @override
+  _InputState createState() => _InputState(this.type, this.description);
+}
+
+class _InputState extends State<InputTextWidget> {
+  
+  final TextInputType type;
+  final String description;
+
+  _InputState(this.type, this.description);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
-    var fibonacci = Fibonacci(0, 1);
-    String numbers = fibonacci.printNumbers();
-
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Fibonacci 10 numbers:' + numbers,
-            ),
-          ],
+    return Container(
+      child: TextField(
+        keyboardType: this.type,
+        decoration: InputDecoration(
+          labelText: this.description,
+          hintText: "Write a value"
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
+    );
+  }
+}
+
+class DropdownWidget extends StatefulWidget {
+  @override
+  _DropdownWidgetState createState() => _DropdownWidgetState();
+}
+
+class _DropdownWidgetState extends State<DropdownWidget> {
+
+  String selected = "Abogado";
+  var elements = ["Abogado", "Medico", "Ingeniero"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: DropdownButton(
+        value: selected,
+        items: elements.map((String item)
+        {
+          return DropdownMenuItem(
+            value: item,
+            child: Text(item)
+          );
+        }).toList(),
+        onChanged: (String value)
+        {
+          setState(() {
+            selected = value;
+          });
+        }
+      )
+    );
+  }
+}
+
+class CheckboxInput extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<CheckboxInput> {
+
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CheckboxListTile(
+        title: Text("Acepto terminos y condiciones"),
+        value: selected,
+        onChanged: (bool value)
+        {
+            setState(() {
+              selected = value;
+            });
+        },
+      )
     );
   }
 }

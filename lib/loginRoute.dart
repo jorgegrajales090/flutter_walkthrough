982e7448728d4e4bd5d259c262ba9e8a1a8e200e
Jorge Grajales <jorgegrajales090@outlook.com>
2021-02-12 20:29:01 -0500

    Agregando input controllers

--- lib/loginRoute.dart
+++ lib/loginRoute.dart
@@ -9,14 +9,12 @@ class LoginRoute extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text("Profile")), body: LoginWidget());
   }
-
-  void changeRoute(BuildContext context) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => HomeRoute()));
-  }
 }
 
 class LoginWidget extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,18 +30,25 @@ class LoginWidget extends StatelessWidget {
                     height: 90,
                     child: Image(
                         image: NetworkImage("http://bit.ly/flutter_tiger")))),
-            InputTextWidget(TextInputType.emailAddress, "Email"),
-            InputTextWidget(TextInputType.visiblePassword, "Password"),
+            InputTextWidget(
+                TextInputType.emailAddress, "Email", usernameController),
+            InputTextWidget(
+                TextInputType.visiblePassword, "Password", passwordController),
             TextButton(
                 child: Text("Login"),
                 style: TextButton.styleFrom(
                     backgroundColor: Colors.blue, primary: Colors.white),
-                onPressed: () async {})
+                onPressed: () async {
+                  login(context);
+                })
           ],
         ));
   }
 
-  Future<String> getData() async {
-    return Future.delayed(Duration(seconds: 8), () => "Delayed by 8 seconds");
+  void login(BuildContext context) async {}
+
+  void changeRoute(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomeRoute()));
   }
 }
--- lib/widgets/InputTextWidget.dart
+++ lib/widgets/InputTextWidget.dart
@@ -3,23 +3,27 @@ import 'package:flutter/material.dart';
 class InputTextWidget extends StatefulWidget {
   final TextInputType type;
   final String description;
+  final TextEditingController controller;
 
-  InputTextWidget(this.type, this.description);
+  InputTextWidget(this.type, this.description, this.controller);
 
   @override
-  _InputState createState() => _InputState(this.type, this.description);
+  _InputState createState() =>
+      _InputState(this.type, this.description, this.controller);
 }
 
 class _InputState extends State<InputTextWidget> {
   final TextInputType type;
   final String description;
+  final TextEditingController controller;
 
-  _InputState(this.type, this.description);
+  _InputState(this.type, this.description, this.controller);
 
   @override
   Widget build(BuildContext context) {
     return Container(
         child: TextField(
+      controller: this.controller,
       keyboardType: this.type,
       decoration: InputDecoration(
           labelText: this.description, hintText: "Write a value"),
